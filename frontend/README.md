663bb0f8d905f714a8efb565116c80ca42413d69
Nuriya <n.m.mustafina@gmail.com>
2022-09-20 10:40:58 +0600

    init frontend

---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+# Music catalog
+## Homework 85
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Music catalog
+        </div>
+    );
+};
+
+export default App;
