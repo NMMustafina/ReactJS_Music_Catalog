318b3474c490200e445d57d53d749842d4e42f00
NMMustafina <n.m.mustafina@gmail.com>
2022-10-12 16:57:57 +0600

    HW#91 - added facebookLogin route

--- api/app/users.js
+++ api/app/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const axios = require("axios");
+const {nanoid} = require('nanoid');
 const User = require('../models/User');
+const config = require('../config');
 
 const router = express.Router();
 
@@ -36,6 +39,45 @@ router.post('/sessions', async (req, res) => {
         res.send({message: 'Username and password correct!', user});
     } catch {
         res.sendStatus(500);
+    }
+});
+
+router.post('/facebookLogin', async (req, res) => {
+    const inputToken = req.body.accessToken;
+    const accessToken = config.facebook.appId + '|' + config.facebook.appSecret;
+
+    const debugTokenUrl = `https://graph.facebook.com/debug_token?input_token=${inputToken}&ac
+cess_token=${accessToken}`;
+
+    try {
+        const response = await axios.get(debugTokenUrl);
+
+        if (response.data.data.error) {
+            return res.status(401).send({message: 'Facebook token incorrect!'});
+        }
+
+        if (req.body.id !== response.data.data.user_id) {
+            return res.status(401).send({message: 'Wrong user ID'});
+        }
+
+        let user = await User.findOne({facebookId: req.body.id});
+
+        if (!user) {
+            user = new User({
+                email: req.body.email,
+                password: nanoid(),
+                facebookId: req.body.id,
+                displayName: req.body.name,
+            });
+        }
+
+        user.generateToken();
+        await user.save({validateBeforeSave: false});
+
+        return res.send({message: 'Login or register successful', user});
+
+    } catch (e) {
+        return res.status(401).send({message: 'Facebook token incorrect'});
     }
 });
 
--- api/config.js
+++ api/config.js
@@ -8,5 +8,9 @@ module.exports = {
   mongo: {
     db: 'mongodb://localhost/player',
     options: {useNewUrlParser: true},
+  },
+  facebook: {
+    appId: '931048021618155',
+    appSecret: process.env.FACEBOOK_APP_SECRET,
   }
 };
